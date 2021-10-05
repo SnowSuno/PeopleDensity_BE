b20a93d1320279fd285c2852c8bcaa7cfd26b40a
SnowSuno <percy3368@gmail.com>
2021-10-05 14:31:03 +0900

    deploy : delete fastapi options

--- app/core/models/database.py
+++ app/core/models/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True,
-                       connect_args={"check_same_thread": False}  # SQLite only
+                       # connect_args={"check_same_thread": False}  # SQLite only
                        )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
