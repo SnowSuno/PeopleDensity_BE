49b4e290cf1c0543c0d90a91d3d054935b35ea49
SnowSuno <percy3368@gmail.com>
2021-10-07 15:12:24 +0900

    refactor : refactor model base __tablename__ auto generation

--- app/models/place.py
+++ app/models/place.py
@@ -3,8 +3,6 @@ from sqlalchemy import Column, String, Integer
 from app.database.base import Base
 
 class Place(Base):
-    __tablename__ = 'place'
-
     id = Column(Integer, primary_key=True, index=True)
     key = Column(String, unique=True, index=True)
     token = Column(String)
